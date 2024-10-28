07d33d924df1b0ba7f2cb17bd1406d1c1798408c
Alejandra Paz Rivas <alejandra.paz@aktios.com>
2024-10-28 23:26:58 +0100

    Proyecto Gestión trabajadores multiusuario Versión 3.

---
+++ Unit 4 - OOP coding Good Practices/GestionTrabajadores/Versión 3/GestionTrabajadoresSolution/GestionTrabajadoresConsole/Enum/EnumStatusTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionTrabajadoresConsole.Enum
+{
+    internal enum EnumStatusTask
+    {
+        ToDo,
+        Doing,
+        Done
+    }
+}
